53a7a38f6de4f667afffac5b0be5ac4b937e69c1
Cody Seibert <cseibert113@gmail.com>
2014-12-09 23:31:45 -0500

    Added gym to routes

--- src/routes.js
+++ src/routes.js
@@ -9,11 +9,18 @@
 var app = require('./app');
 
 var GradesController = require('./controllers/GradesController');
+var GymsController = require('./controllers/GymsController');
 
 var RouteToControllerBinder = function () {
     'use strict';
     app.get('/api/grades/boulder', GradesController.getBoulderGrades);
     app.get('/api/grades/rope', GradesController.getRopeGrades);
+
+    app.get('/api/gyms', GymsController.getGyms);
+    app.get('/api/gyms/:id', GymsController.getGym);
+    app.post('/api/gyms/:id', GymsController.updateGym);
+    app.post('/api/gyms', GymsController.createGym);
+    app.delete('/api/gyms/:id', GymsController.deleteGym);
 };
 
 module.exports = new RouteToControllerBinder();

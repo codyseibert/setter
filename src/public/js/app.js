54e503e3b55b9c766751adf9ba9fd70316d4ce35
System Administrator <root@unknown14109fdfa08b.attlocal.net>
2015-01-05 02:14:53 -0500

    fixed side nav bug on mobile devices by adding foundation min js from CDNJS and removing foundationa angular directives, began adding new coloring to side nav menu and styling those

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,6 @@
 /*global angular: false, btoa: false, Chart: false, confirm: false */
 
-angular.module('SETTER', ['ngRoute', 'ngCookies', 'chart.js', 'mm.foundation', 'mm.foundation.offcanvas', 'ngUpload'])
+angular.module('SETTER', ['ngRoute', 'ngCookies', 'chart.js', 'ngUpload'])
     .config(['$routeProvider', function ($routeProvider) {
         'use strict';
 

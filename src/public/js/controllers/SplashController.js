2c8457e72f3a67015eade96fe295e79cf750daf5
Cody Seibert <cseibert113@gmail.com>
2015-02-10 23:29:53 -0500

    Added a splash screen template / controller

---
+++ src/public/js/controllers/SplashController.js
@@ -0,0 +1,21 @@
+/*jslint nomen: true */
+/*global angular: false, btoa: false, console: false, confirm: false, moment: false */
+
+angular.module('SETTER')
+  .controller('SplashController', [
+    '$scope',
+    '$sce',
+    'BlogService',
+    'DateFormatService',
+    'LoginService',
+    function SplashController(
+      $scope,
+      $sce,
+      BlogService,
+      DateFormatService,
+      LoginService
+    ) {
+      'use strict';
+
+
+    }]);

f1ceda6c849e18f606444949b46a53a8b422d1e8
Cody Seibert <cseibert113@gmail.com>
2015-05-23 13:44:55 -0400

    best rated takes into account the number of ratings

--- src/dao/GymsDao.js
+++ src/dao/GymsDao.js
@@ -21,7 +21,8 @@ var GymsDao = function () {
     'use strict';
 
     var BEST_ROUTES_LIMIT = 15,
-        NEWEST_ROUTES_LIMIT = 15;
+        NEWEST_ROUTES_LIMIT = 15,
+        COUNT_THRESH = 4;
 
     this.getGym = function (pGymId, pCallback) {
         theDaoHelper.executeQuery(
@@ -196,13 +197,13 @@ var GymsDao = function () {
     */
     this.getBestRatedBoulder = function (pGymId, pCallback) {
         theDaoHelper.executeQuery(
-            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, bg.name AS boulder_grade ' +
+            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) AS count, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, bg.name AS boulder_grade ' +
                 'FROM routes ro ' +
                 'INNER JOIN ratings ra ON ra.route_id = ro.id ' +
                 'INNER JOIN colors c ON ro.color_id = c.id ' +
                 'INNER JOIN walls w ON w.id = ro.wall_id ' +
                 'INNER JOIN boulder_grades bg ON ro.boulder_grade_id = bg.id ' +
-                'WHERE ro.active = true AND w.gym_id = ? ' +
+                'WHERE ro.active = true AND w.gym_id = ? AND (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) > ' + COUNT_THRESH + ' ' +
                 'GROUP BY ro.id ' +
                 'ORDER BY rating DESC LIMIT ' + BEST_ROUTES_LIMIT,
             [pGymId],
@@ -213,13 +214,13 @@ var GymsDao = function () {
 
     this.getBestRatedTopRope = function (pGymId, pCallback) {
         theDaoHelper.executeQuery(
-            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, ro.date, rg.name AS rope_grade ' +
+            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) AS count, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, ro.date, rg.name AS rope_grade ' +
                 'FROM routes ro ' +
                 'INNER JOIN ratings ra ON ra.route_id = ro.id ' +
                 'INNER JOIN colors c ON ro.color_id = c.id ' +
                 'INNER JOIN walls w ON w.id = ro.wall_id ' +
                 'INNER JOIN rope_grades rg ON ro.toprope_grade_id = rg.id ' +
-                'WHERE ro.active = true AND w.gym_id = ? ' +
+                'WHERE ro.active = true AND w.gym_id = ? AND (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) > ' + COUNT_THRESH + ' ' +
                 'GROUP BY ro.id ' +
                 'ORDER BY rating DESC LIMIT ' + BEST_ROUTES_LIMIT,
             [pGymId],
@@ -230,13 +231,13 @@ var GymsDao = function () {
 
     this.getBestRatedLead = function (pGymId, pCallback) {
         theDaoHelper.executeQuery(
-            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, rg.name AS rope_grade ' +
+            'SELECT ro.id, w.name AS zone_name, AVG(ra.rating) AS rating, (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) AS count, c.value, w.id AS wall_id, w.gym_id AS gym_id, ro.date, rg.name AS rope_grade ' +
                 'FROM routes ro ' +
                 'INNER JOIN ratings ra ON ra.route_id = ro.id ' +
                 'INNER JOIN colors c ON ro.color_id = c.id ' +
                 'INNER JOIN walls w ON w.id = ro.wall_id ' +
                 'INNER JOIN rope_grades rg ON ro.lead_grade_id = rg.id ' +
-                'WHERE ro.active = true AND w.gym_id = ? ' +
+                'WHERE ro.active = true AND w.gym_id = ? AND (SELECT COUNT(*) FROM ratings r WHERE r.route_id = ro.id) > ' + COUNT_THRESH + ' ' +
                 'GROUP BY ro.id ' +
                 'ORDER BY rating DESC LIMIT ' + BEST_ROUTES_LIMIT,
             [pGymId],

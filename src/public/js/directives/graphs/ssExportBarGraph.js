cac8aa1412ecc708efed4fb3de9d1b296caa57fa
Cody Seibert <cseibert113@gmail.com>
2015-05-27 23:59:29 -0400

    trying to fix the export feature

--- src/public/js/directives/graphs/ssExportBarGraph.js
+++ src/public/js/directives/graphs/ssExportBarGraph.js
@@ -18,17 +18,55 @@ angular.module('SETTER')
               templateUrl: 'templates/graphs/ssExportBarGraph.tpl.html',
               controller: function($scope) {
                 $scope.export = function () {
-                    var canvas,
-                        image,
-                        doc,
-                        JsPDF = jsPDF;
-                    canvas = $('#' + $scope.canvasId).find('canvas').get(0);
+
+                  // Takes an SVG element as target
+                  function svg_to_png_data(target) {
+                    var ctx, mycanvas, svg_data, img, child;
+
+                    // Flatten CSS styles into the SVG
+                    for (var i = 0; i < target.childNodes.length; i++) {
+                      child = target.childNodes[i];
+                      var cssStyle = window.getComputedStyle(child);
+                      if(cssStyle){
+                         child.style.cssText = cssStyle.cssText;
+                      }
+                    }
+
+                    // Construct an SVG image
+                    svg_data = '<svg xmlns="http://www.w3.org/2000/svg" width="' + target.offsetWidth +
+                               '" height="' + target.offsetHeight + '">' + target.innerHTML + '</svg>';
+                    console.log(svg_data);
+                    img = new Image();
+                    img.src = "data:image/svg+xml," + encodeURIComponent(svg_data);
+
+                    // Draw the SVG image to a canvas
+                    mycanvas = document.createElement('canvas');
+                    mycanvas.width = target.offsetWidth;
+                    mycanvas.height = target.offsetHeight;
+                    ctx = mycanvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0);
+
+                    // Return the canvas's data
+                    return mycanvas.toDataURL("image/png");
+                  }
+
+                  // Takes an SVG element as target
+                  function svg_to_png_replace(target) {
+                    var data, image;
+                    data = svg_to_png_data(target);
                     image = new Image();
-                    image.src = canvas.toDataURL("image/png");
-                    doc = new JsPDF();
+                    image.src = data;
+                    // var logo = new Image();
+                    // logo.src = 'images/setter_print_logo.png'
+                    var doc = new jsPDF();
                     doc.text(20, 20, $scope.title);
-                    doc.addImage(image, 15, 40, 180, 160);
+                    doc.addImage(image, 'png', 15, 40, 180, 160);
+                    // doc.addImage(logo, 'png', 10, 10, 100, 100);
                     doc.save($scope.docName);
+                  }
+
+                  svg_to_png_replace($('#' + $scope.canvasId).find('.ct-chart > svg')[0]);
+
                 };
               },
               link: function(scope, element, attrs) {

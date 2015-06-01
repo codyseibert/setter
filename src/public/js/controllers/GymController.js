0d475b1db6a49ef78daf3ca212fa0723b7595cc2
Cody Seibert <cseibert113@gmail.com>
2015-05-31 23:34:25 -0400

    removed testing log statements

--- src/public/js/controllers/GymController.js
+++ src/public/js/controllers/GymController.js
@@ -124,7 +124,6 @@ angular.module('SETTER')
             GymsService.getLatestComments($scope.gymId)
               .success(function (pData) {
                   $scope.comments = pData;
-                  console.log($scope.comments);
               });
 
             GymsService.getGymImage($scope.gymId, function (pData) {

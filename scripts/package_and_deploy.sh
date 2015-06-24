b5af283e82cd455b25bb0102440926374f1f8df8
Cody Seibert <cseibert113@gmail.com>
2015-06-23 22:23:12 -0400

    version bump

--- scripts/package_and_deploy.sh
+++ scripts/package_and_deploy.sh
@@ -1,4 +1,4 @@
-SETTER_VERSION="1.1.2"
+SETTER_VERSION="1.1.3"
 LOCAL_SETTER_DIR="/home/cseibert/Workspace/setter"
 LOCAL_SETTER_APP_DIR="$LOCAL_SETTER_DIR/build/app"
 LOCAL_SETTER_BUILD_DIR="/home/cseibert/Workspace/setter/artifacts"
